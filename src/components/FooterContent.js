89794d2ce9f499423db66b99175331241f33c53c
Jose Daniel Romero <danyrome12@gmail.com>
2023-07-18 18:11:58 -0500

    Mejora del Footer y diseno de la pagina 404

--- src/components/FooterContent.js
+++ src/components/FooterContent.js
@@ -1,17 +1,19 @@
 import { useContext } from 'react'
 import { ThemeContext } from '../App'
 import '../stylesheets/FooterContent.css'
+import githubIcon from '../images/github-icon.png'
 
 function FooterContent() {
 
   const { darkMode } = useContext(ThemeContext)
   const handleThemeFooterBox = darkMode == false ? 'footer' : 'footer dark'
+  const handleThemeFooterIcon = darkMode == false ? 'github-icon' : 'github-icon dark'
 
   return (
     <footer className={handleThemeFooterBox}>
-      
+      <p className='footer-text'>created by José Daniel Romero <a title='Github page' href='https://github.com/JoseDanielRomero' target='_blank'><img src={githubIcon} className={handleThemeFooterIcon} alt='Github page'/></a> - frontendmentor.io</p>
     </footer>
   )
 }
 
-export default FooterContent
+export default FooterContent;
--- src/routes/ErrorContent.js
+++ src/routes/ErrorContent.js
@@ -3,17 +3,24 @@ import { ThemeContext } from '../App'
 import FooterContent from '../components/FooterContent';
 import HeaderContent from '../components/HeaderContent';
 import '../stylesheets/ErrorContent.css'
+import errorIcon from '../images/404.png'
+import { Link } from 'react-router-dom';
 
 function ErrorContent() {
 
   const { darkMode } = useContext(ThemeContext)
-  const handleThemeMain = darkMode == false ? 'main-country' : 'main-country dark'
+  const handleThemeMain = darkMode == false ? 'main-error' : 'main-error dark'
+  const handleThemeIconError = darkMode == false ? 'main-error-image' : 'main-error-image dark'
+  const handleThemeTitleError = darkMode == false ? 'main-error-title' : 'main-error-title dark'
+  const handleThemeLink = darkMode == false ? 'main-error-link' : 'main-error-link dark'
 
   return (
     <div className='ErrorPage'>
       <HeaderContent />
       <main className={handleThemeMain}>
-
+        <img className={handleThemeIconError} src={errorIcon} />
+        <h2 className={handleThemeTitleError}>Not the country you were looking for.</h2>
+        <Link to='/' className={handleThemeLink}>Back Home</Link>
       </main>
       <FooterContent />
     </div>
